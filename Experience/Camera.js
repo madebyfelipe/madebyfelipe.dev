ac8bfc686120ae425110231f91f3b436f11dc78a
Felipe Silva <contato@madebyfelipe.dev>
2023-04-01 21:10:52 -0300

    #

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -1,13 +1,36 @@
 import Experience from "./Experience.js";
 
-export default class Camera{
-    constructor(){
-        this.experience = new Experience();
-        this.sizes = this.experience.sizes;
-        this.scene = this.experience.scene;
-        this.canvas = this.experience.canvas;
+export default class Camera {
+  constructor() {
+    this.experience = new Experience();
+    this.sizes = this.experience.sizes;
+    this.scene = this.experience.scene;
+    this.canvas = this.experience.canvas;
 
-        this.createPerspectiveCamera();
-        this.createOrthoCamera();
-    }
+    this.createPerspectiveCamera();
+    this.createOrthographicCamera();
+  }
+
+  createPerspectiveCamera() {
+    this.perspectiveCamera = new THREE.PerspectiveCamera(
+      35,
+      this.sizes.aspect,
+      0.1,
+      1000
+    );
+    this.scene.add(this.perspectiveCamera);
+  }
+
+  createOrthographicCamera() {
+    this.frustrum = 5;
+    this.orthographicCamera = new THREE.OrthographicCamera(
+      (-this.sizes.aspect * this.frustrum) / 2,
+      (this.sizes.aspect * this.frustrum) / 2,
+      this.frustrum / 2,
+      -this.frustrum / 2,
+      -100,
+      100
+    );
+    this.scene.add(this.orthographicCamera);
+  }
 }
